4cf350a58d0a2a0522c4015a05c72a42c9fca297
Roshan T <127504918+Roshan835@users.noreply.github.com>
2023-05-12 09:40:11 +0530

    Update sentiment-analysis.py

--- sentiment-analysis.py
+++ sentiment-analysis.py
@@ -1,25 +1,41 @@
-import pandas as pd
 import streamlit as st
-review = pd.read_csv("reviews.csv")
-review = review.rename(columns = {"text": "review"},inplace = False)
+import pandas as pd
 from sklearn.model_selection import train_test_split
-X = review.review
-y = review.polarity
-X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.6, random_state = 1)
 from sklearn.feature_extraction.text import CountVectorizer
-vector = CountVectorizer(stop_words = "english", lowercase = False)
-vector.fit(X_train)
-X_transformed = vector.transform(X_train)
-X_transformed.toarray()
-X_test_transformed = vector.transform(X_test)
 from sklearn.naive_bayes import MultinomialNB
-naivebayes = MultinomialNB()
-naivebayes.fit(X_transformed, y_train)
 import pickle
-saved_model = pickle.dumps(naivebayes)
-s = pickle.loads(saved_model)
-st.header('Sentiment Analyzer')
-input = st.text_area("Enter the word", value="")
-vec = vector.transform([input]).toarray()
-if st.button("Analyse"):
-    st.write(str(list(s.predict(vec))[0]).replace("0","NEGATIVE").replace("1","POSITIVE")))
+def trainmodel():
+    review=pd.read_csv('reviews.csv')
+    review=review.rename(columns={'text':'review'},inplace=False)
+
+    X = review.review
+    y = review.polarity
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size = 0.6, random_state = 1)
+    vector = CountVectorizer(stop_words = 'english',lowercase=False)
+    vector.fit(X_train)
+    print(vector.vocabulary_)
+    X_transformed = vector.transform(X_train)
+    X_transformed.toarray()
+
+    X_test_transformed = vector.transform(X_test)
+    
+    naivebayes=MultinomialNB()
+    naivebayes.fit(X_transformed, y_train)
+    saved_model = pickle.dumps(naivebayes)
+    s = pickle.loads(saved_model)
+    return s,vector
+s,vector=trainmodel()
+
+def predict(review1):
+    vec = vector.transform([review1]).toarray()
+    sentiment = str(list(s.predict(vec))[0]).replace('0', 'NEGATIVE').replace('1', 'POSITIVE')
+    return sentiment   
+
+   
+st.header('Sentiment Analyser classifier')
+
+
+review1=st.text_area("Enter the text")
+if st.button ('Analyse'):
+    st.write(predict(review1))
